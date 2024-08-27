51a19207144f51009756095dbacaaba4128644d4
Kamal <kamal0000027@gmail.com>
2024-08-27 23:01:49 +0530

    slider

--- EducationalWebsite/script.js
+++ EducationalWebsite/script.js
@@ -12,7 +12,39 @@ let faq = document.querySelectorAll('.faq-questions')
 Array.from(faq).forEach((e) => {
     e.addEventListener('click', () => {
         e.classList.toggle('faq-open')
-        
     })
 })
+
+// Slides Testimonial
+
+let testimonial = document.querySelectorAll('.testimonial')
+Array.from(testimonial).forEach((e, i) => {
+    e.style.left = `${i * 40}%`
+})
+
+let prev = document.querySelector('.prev')
+let next = document.querySelector('.next')
+
+let count = 0
+
+prev.addEventListener('click', () => {
+    if (count > 0) {
+        count--
+        Array.from(testimonial).forEach((e) => {
+            e.style.transform = `translateX(-${100 * count}%)`
+        })
+    }
+})
+
+next.addEventListener('click', () => {
+    if (count < testimonial.length - 2) {
+        count++
+        Array.from(testimonial).forEach((e) => {
+            e.style.transform = `translateX(${-100 * count}%)`
+        })
+    }
+})
+
+
+
 

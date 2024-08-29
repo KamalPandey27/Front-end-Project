e40ca026e9b094d4b77588e119cf9dc37ba0e521
Kamal <kamal0000027@gmail.com>
2024-08-29 21:28:28 +0530

    some other work on this project

--- EducationalWebsite/script.js
+++ EducationalWebsite/script.js
@@ -27,7 +27,24 @@ let next = document.querySelector('.next')
 
 let count = 0
 
+let testimonialInterval = setInterval(() => {
+    if (count < testimonial.length - 1) {
+        Array.from(testimonial).forEach((e) => {
+            e.style.transform = `translateX(${-100 * count}%)`
+        })
+        count++
+    }
+
+    if (count == testimonial.length - 1) {
+        count = 0
+    }
+
+}, 1000);
+
 prev.addEventListener('click', () => {
+
+    clearInterval(testimonialInterval)
+
     if (count > 0) {
         count--
         Array.from(testimonial).forEach((e) => {
@@ -37,6 +54,9 @@ prev.addEventListener('click', () => {
 })
 
 next.addEventListener('click', () => {
+
+    clearInterval(testimonialInterval)
+
     if (count < testimonial.length - 2) {
         count++
         Array.from(testimonial).forEach((e) => {
